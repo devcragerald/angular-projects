a8d6d01e6dcd95303aca27de56bdaed22698e72e
antonny cr <devcragerald@gmail.com>
2022-07-24 19:53:32 +0200

    feature[angular]: add styles

--- 01-basics/src/app/app.component.ts
+++ 01-basics/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+
+  // template: `
+  //   <h1>yooloo</h1>
+  // `
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  title = '01-basics';
+
 }
